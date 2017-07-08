bb4700da62d07497741bb27816015262661ad046
Samuel DA MOTA <da.mota.sam@gmail.com>
2017-07-08 22:15:06 +0200

    moved the port listings into its own file

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,6 @@
 extern crate clap;
-extern crate midir;
 
-// Todo find a way to factorise print_input_ports and print_output_ports.
-// Maybe with a macro
-fn print_input_ports() {
-    // listing input midi ports
-    let input = midir::MidiInput::new("");
-    match input {
-        Err(e) => { println!("Failed to list input ports: {}", e) }
-        Ok(input) => {
-            let nb_ports = input.port_count();
-            match nb_ports {
-                0 => { println!("Sorry: no input midi port found"); }
-                1 => {
-                    println!("! input port found:");
-                    let name_res = input.port_name(0);
-                    match name_res {
-                        Ok(name) => println!("  0 -> {}", name),
-                        Err(e) => println!("Error while retrieving port's name: {}", e),
-                    }
-                }
-                _ => {
-                    println!("{} input ports found:", nb_ports);
-                    for i in 0..nb_ports {
-                        let name_res = input.port_name(i);
-                        match name_res {
-                            Ok(name) => println!("  {} -> {}", i, name),
-                            Err(e) => println!("Error while retrieving port {}'s name: {}", i, e),
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-fn print_output_ports() {
-    // listing output midi ports
-    let output = midir::MidiOutput::new("");
-    match output {
-        Err(e) => { println!("Failed to list output ports: {}", e) }
-        Ok(output) => {
-            let nb_ports = output.port_count();
-            match nb_ports {
-                0 => { println!("Sorry: no output midi port found"); }
-                1 => {
-                    println!("! output port found:");
-                    let name_res = output.port_name(0);
-                    match name_res {
-                        Ok(name) => println!("  0 -> {}", name),
-                        Err(e) => println!("Error while retrieving port's name: {}", e),
-                    }
-                }
-                _ => {
-                    println!("{} output ports found:", nb_ports);
-                    for i in 0..nb_ports {
-                        let name_res = output.port_name(i);
-                        match name_res {
-                            Ok(name) => println!("  {} -> {}", i, name),
-                            Err(e) => println!("Error while retrieving port {}'s name: {}", i, e),
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+mod ports_printer;
 
 fn main() {
     let input_midi_port_option_name = "input port";
@@ -101,9 +36,9 @@ fn main() {
         .get_matches();
 
     if options.is_present(list_option_name) {
-        print_input_ports();
+        ports_printer::print_inputs();
         println!();
-        print_output_ports();
+        ports_printer::print_outputs();
         return;
     }
 
---
+++ src/ports_printer.rs
@@ -0,0 +1,68 @@
+extern crate midir;
+
+
+// Todo find a way to factorise print_input_ports and print_output_ports.
+// Maybe with a macro
+pub fn print_inputs() {
+    // listing input midi ports
+    let input = midir::MidiInput::new("");
+    match input {
+        Err(e) => { println!("Failed to list input ports: {}", e) }
+        Ok(input) => {
+            let nb_ports = input.port_count();
+            match nb_ports {
+                0 => { println!("Sorry: no input midi port found"); }
+                1 => {
+                    println!("! input port found:");
+                    let name_res = input.port_name(0);
+                    match name_res {
+                        Ok(name) => println!("  0 -> {}", name),
+                        Err(e) => println!("Error while retrieving port's name: {}", e),
+                    }
+                }
+                _ => {
+                    println!("{} input ports found:", nb_ports);
+                    for i in 0..nb_ports {
+                        let name_res = input.port_name(i);
+                        match name_res {
+                            Ok(name) => println!("  {} -> {}", i, name),
+                            Err(e) => println!("Error while retrieving port {}'s name: {}", i, e),
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn print_outputs() {
+    // listing output midi ports
+    let output = midir::MidiOutput::new("");
+    match output {
+        Err(e) => { println!("Failed to list output ports: {}", e) }
+        Ok(output) => {
+            let nb_ports = output.port_count();
+            match nb_ports {
+                0 => { println!("Sorry: no output midi port found"); }
+                1 => {
+                    println!("! output port found:");
+                    let name_res = output.port_name(0);
+                    match name_res {
+                        Ok(name) => println!("  0 -> {}", name),
+                        Err(e) => println!("Error while retrieving port's name: {}", e),
+                    }
+                }
+                _ => {
+                    println!("{} output ports found:", nb_ports);
+                    for i in 0..nb_ports {
+                        let name_res = output.port_name(i);
+                        match name_res {
+                            Ok(name) => println!("  {} -> {}", i, name),
+                            Err(e) => println!("Error while retrieving port {}'s name: {}", i, e),
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
