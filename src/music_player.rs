948eb3c09906e8a1dcae940c615135d8ece735f1
Samuel DA MOTA <da.mota.sam@gmail.com>
2017-09-04 22:18:34 +0200

    initialises the UI and print two lines (doesn't draw the keyboard yet)

--- src/music_player.rs
+++ src/music_player.rs
@@ -1,8 +1,10 @@
 extern crate midir;
+extern crate rustbox;
 
 use std::error::Error;
 use std;
 use utils;
+use self::rustbox::{RustBox, Event};
 
 fn play_music(midi_out: &mut midir::MidiOutputConnection, event: &utils::MusicEvent)
 {
@@ -14,6 +16,20 @@ fn play_music(midi_out: &mut midir::MidiOutputConnection, event: &utils::MusicEv
             }
         }
     }
+}
+
+fn init_ref_pos(width: usize, height: usize) -> (usize, usize) {
+    let keyboard_height = 8;
+    let keyboard_width = 188;
+    ((width - keyboard_width) / 2, (height - keyboard_height) / 2)
+}
+
+fn update_screen(ui: &RustBox, ref_x: usize, ref_y: usize)
+{
+    ui.clear();
+    ui.print(ref_x, ref_y + 10, rustbox::RB_BOLD, rustbox::Color::Magenta, rustbox::Color::Default, "press <CTRL + q> to quit");
+    ui.print(ref_x, ref_y + 11, rustbox::RB_BOLD, rustbox::Color::Magenta, rustbox::Color::Default, "press <space> to pause/unpause");
+    ui.present();
 }
 
 pub fn play(song: utils::Song, midi_output_port: u32) {
@@ -32,10 +48,19 @@ pub fn play(song: utils::Song, midi_output_port: u32) {
     }
 
     let mut conn_out = conn_out.unwrap();
+    let ui = RustBox::init(Default::default());
+    if let Err(e) = ui {
+        println!("Failed to initialise the user interface (rustbox): {}", e.description());
+        return ();
+    };
+
+    let ui = ui.unwrap();
+    let (mut x, mut y) = init_ref_pos(ui.width(), ui.height());
 
     let nb_events = song.len();
     for i in 0 .. nb_events {
-        println!("playing event {} out of {}", i, nb_events);
+
+        update_screen(&ui, x, y);
         let current_event = &song[i];
         play_music(&mut conn_out, current_event);
 
@@ -61,7 +86,24 @@ pub fn play(song: utils::Song, midi_output_port: u32) {
                         remaining_time
                     }
                 };
-                std::thread::sleep(time_to_sleep);
+
+
+                match ui.peek_event(time_to_sleep, false) {
+                    Ok(Event::NoEvent)
+                    | Ok(Event::MouseEvent(_, _, _)) => (),
+                    Ok(Event::KeyEventRaw(_, _, _)) => panic!("Raw event received, whereas the raw parameter to peek_event was set to false!"),
+                    Ok(Event::ResizeEvent(w, h)) => {
+                        if (w < 0) || (h < 0) {
+                            panic!("new window size has negtive components. Can't happen after a successful init!");
+                        }
+                        let (this_x, this_y) = init_ref_pos(w as usize, h as usize);
+                        x = this_x;
+                        y = this_y;
+                    },
+                    Ok(Event::KeyEvent(_)) => (), // TODO handle key events.
+                    Err(e) => { println!("Error occured in rustbox: {}", e.description()); return (); },
+                };
+
             }
         }
     }
